0fde6212d744f9ee8a85226170dc68a76132703b
jhsiaomei <jw1314@gmail.com>
2015-12-16 20:44:15 -0600

    created controller methods and views for events, fields, groups

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -24,14 +24,31 @@ class EventsController < ApplicationController
   end
 
   def show
+    @event = Event.find_by(id: params[:id])
   end
 
   def edit
+    @event = Event.find_by(id: params[:id])
   end
 
   def update
+    @event = Event.find_by(id: params[:id])
+    @event.update(
+      name: params[:name],
+      event_type: params[:event_type],
+      date_start: params[:date_start],
+      date_stop: params[:date_stop],
+      time_start: params[:time_start],
+      time_stop: params[:time_stop],
+      description: params[:description],
+      field_id: params[:field_id],
+      )
+    redirect_to "/events/#{@event.id}"
   end
 
   def destroy
+    event = Event.find_by(id: params[:id])
+    event.destroy
+    redirect_to '/events'
   end
 end
--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,22 +1,56 @@
 class GroupsController < ApplicationController
   def index
+    @groups = Group.all
   end
 
   def new
+    @group = Group.create(
+      name: params[:name],
+      description: params[:description],
+      field_id_1: params[:field_id_1],
+      field_id_2: params[:field_id_2],
+      field_id_3: params[:field_id_3],
+      group_type: params[:group_type],
+      )
   end
 
   def create
+    @group = Group.create(
+      name: params[:name],
+      description: params[:description],
+      field_id_1: params[:field_id_1],
+      field_id_2: params[:field_id_2],
+      field_id_3: params[:field_id_3],
+      group_type: params[:group_type],
+      )
+    redirect_to "/groups/#{@group.id}"
   end
 
   def show
+    @group = Group.find_by(id: params[:id])
   end
 
   def edit
+    @group = Group.find_by(id: params[:id])
   end
 
   def update
+    @group = Group.find_by(id: params[:id])
+    @group.update(
+      name: params[:name],
+      admin_id: params[:admin_id],
+      description: params[:description],
+      field_id_1: params[:field_id_1],
+      field_id_2: params[:field_id_2],
+      field_id_3: params[:field_id_3],
+      group_type: params[:group_type]
+      )
+    redirect_to "/groups/#{@group.id}"
   end
 
   def destroy
+    @group = Group.find_by(id: params[:id])
+    @group.destroy
+    redirect_to "/groups"
   end
 end
