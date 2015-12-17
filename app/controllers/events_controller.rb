3428dcc40649ef2b8644dc3890f08ac85ea55532
jhsiaomei <jw1314@gmail.com>
2015-12-16 18:27:44 -0600

    installed bootstraps

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,11 +1,26 @@
 class EventsController < ApplicationController
   def index
+    @events = Event.all
   end
 
   def new
+    @event = Event.new
   end
 
   def create
+    @event = Event.new(
+      name: params[:name],
+      event_type: params[:event_type],
+      date_start: params[:date_start],
+      date_stop: params[:date_stop],
+      time_start: params[:time_start],
+      time_stop: params[:time_stop],
+      description: params[:description],
+      field_id: params[:field_id],
+      )
+    @event.save
+
+    redirect_to "/events"
   end
 
   def show
